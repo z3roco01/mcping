8c3711910fc3952636d905eb83b510433ac6783f
z3roco01 <zerocool@e.email>
2025-10-03 19:27:49 -0500

    move socket to a method and not init

--- src/main/kotlin/network/MCSocket.kt
+++ src/main/kotlin/network/MCSocket.kt
@@ -8,14 +8,20 @@ import java.net.Socket
  * @param hostname the hostname or ip of the server
  * @param port the port the server is on ( usually 25565)
  */
-class MCSocket(hostname: String, port: Int, val protocolVersion: Int): Socket(hostname, port) {
+class MCSocket(val hostname: String, val port: Int, val protocolVersion: Int) {
     // a constructor without the port, uses the default port
     constructor(hostname: String, protocolVersion: Int): this(hostname, 25565, protocolVersion)
     // constructor with just the hostname, uses most recent protocol version
     constructor(hostname: String): this(hostname, 772)
 
-    init {
+    fun getStatus() {
+        val sock = Socket(hostname, port)
+        val output = sock.getOutputStream()
         // send a handshake packet immediately
-        HandshakePacket(VarInt(protocolVersion), hostname, port.toUShort(), Packet.Intent.STATUS)
+        val handshake = HandshakePacket(VarInt(protocolVersion), hostname, port.toUShort(), Packet.Intent.STATUS)
+        output.write(handshake.toBytes())
+        output.flush()
+
+        // create and send a status request packet
     }
 }
